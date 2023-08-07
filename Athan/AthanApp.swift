5ea961bb21acad4248b3173673d4ef13679d2e75
eyhdev <alaqsa335@gmail.com>
2023-08-07 15:48:17 +0300

    Initial Commit

---
+++ Athan/AthanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthanApp.swift
+//  Athan
+//
+//  Created by eyh.mac on 7.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AthanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
