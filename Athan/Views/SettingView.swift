6d6bf3adc707ea8be6a6189c3357645152a91616
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2023-10-31 16:49:58 +0300

    init

---
+++ Athan/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Athan
+//
+//  Created by Ali Amer on 10/31/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
---
+++ Athan/Views/testView.swift
@@ -0,0 +1,18 @@
+//
+//  testView.swift
+//  Athan
+//
+//  Created by Ali Amer on 10/31/23.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testView()
+}
